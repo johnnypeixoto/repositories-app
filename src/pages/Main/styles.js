c4082c13e6e526112b620e0c1ac5d3ca0a6e7c65
Johnny <johnny.peixotto@outlook.com>
2020-01-23 22:08:59 -0300

    first commit

---
+++ src/pages/Main/styles.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 700px;
+  background: #fff;
+  border-radius: 6px;
+  padding: 30px;
+  margin: 80px auto;
+  box-shadow: 10px 10px 15px rgba(0, 0, 0, 0.2);
+
+  h1 {
+    font-size: 20px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+
+  svg {
+    margin-right: 10px;
+  }
+`;
+export const Form = styled.form`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: row;
+
+  input {
+    flex: 1;
+    border: 1px solid #eee;
+    padding: 10px 15px;
+    border-radius: 4px;
+    font-size: 16px;
+  }
+`;
+
+export const SubmitButton = styled.button.attrs({
+  type: 'submit',
+})`
+  background: #7159c1;
+  border: 0;
+  padding: 0 10px;
+  margin-left: 10px;
+  border-radius: 4px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
